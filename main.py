8f2cd4ce53d4e5c3c6b38f8f423cdb8018d0da26
HuynhDoTanThanh <74091652+HuynhDoTanThanh@users.noreply.github.com>
2021-07-13 12:45:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import scanner
 import yolov5
 import crop_line_and_ocr
+import sys, getopt
 
-pathImage = 'data'
+pathImage = ''
 
-images = scanner.scanner(pathImage)
-obj = yolov5.object_detection(images)
-dataframe = crop_line_and_ocr.craft_and_ocr(obj)
+def main(argv):
+  global pathImage
+  try:
+    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+  except getopt.GetoptError:
+    print('main.py -i <inputfile>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+        print('main.py -i <inputfile>')
+        sys.exit()
+    elif opt in ("-i", "--ifile"):
+        pathImage = arg
 
-dataframe.to_csv('data.csv', index = True, header=True, encoding = 'utf-8')
+if __name__ == "__main__":
+  main(sys.argv[1:])
 
-print(dataframe)
+  images = scanner.scanner(pathImage)
+  obj = yolov5.object_detection(images)
+  dataframe = crop_line_and_ocr.craft_and_ocr(obj)
+
+  dataframe.to_csv('data.csv', index = True, header=True, encoding = 'utf-8')
+
+  dataframe.head()
