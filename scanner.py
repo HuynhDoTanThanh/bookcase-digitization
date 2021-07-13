76b40f5c4be9149a4f46cb722a7ed7156d7cc99e
HuynhDoTanThanh <74091652+HuynhDoTanThanh@users.noreply.github.com>
2021-07-13 12:46:00 +0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,17 +4,20 @@ import Utlis as utlis
 import os
 
 ########################################################################
-heightImg = 960
-widthImg = 720
+heightImg = 720
+widthImg = 540
 ########################################################################
 
 def scanner(pathImage):
+    # print(pathImage)
+    # ROOT_DIR = os.path.dirname(os.path.abspath(pathImage))
     FJoin = os.path.join
     files = [FJoin(pathImage, f) for f in os.listdir(pathImage)]
 
     images = []
 
     for path in files:
+        print(path)
         img = cv2.imread(path)
         img = cv2.resize(img, None, fx = 0.3, fy = 0.3)  # RESIZE IMAGE
         imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # CONVERT IMAGE TO GRAY SCALE
