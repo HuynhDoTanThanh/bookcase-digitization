961664987da36ca773e02def0f8cd727162d21f2
HuynhDoTanThanh <74091652+HuynhDoTanThanh@users.noreply.github.com>
2021-07-13 13:37:27 +0700

    Update crop_line_and_ocr.py

--- crop_line_and_ocr.py
+++ crop_line_and_ocr.py
@@ -12,7 +12,7 @@ from vietocr.tool.config import Cfg
 import pandas as pd
 import cv2
 
-
+#sort chữ khi crop line để theo thứ tự từ trái sang phải
 def sort_img(regions):
   for i in range(len(regions) - 1):
     min = i
@@ -28,7 +28,7 @@ def sort_img(regions):
 
 def craft_and_ocr(results):
 
-    #load model only use cpu
+    #load model
     refine_net = load_refinenet_model(cuda=True)
     craft_net = load_craftnet_model(cuda=True)
     config = Cfg.load_config_from_name('vgg_transformer')
@@ -41,7 +41,8 @@ def craft_and_ocr(results):
     detector = Predictor(config)
 
     out = []
-
+    
+    #predict ocr
     for info, cache in results:
         ten_sach = ""
         ten_tac_gia = ""
@@ -51,8 +52,10 @@ def craft_and_ocr(results):
         tai_ban = ""
         for key, value in info.items():
             for img in value:
+                #read image
                 image = read_image(img)
-
+                
+                #predict craft
                 prediction_result = get_prediction(
                     image=image,
                     craft_net=craft_net,
@@ -64,19 +67,25 @@ def craft_and_ocr(results):
                     long_size=1280
                 )
                 regions=prediction_result["polys"]
+                
+                #sắp xếp lại ảnh sau khi craft
                 sort_img(regions)
 
                 a = []
+                #chuyển thành ảnh lưu vào mảng a
                 for i in regions:
                   a.append(rectify_poly(image, i))
-
+                
+                #thêm text vảo chuỗi tên sách
                 if key == 0:
                     for i in a:
+                        #nếu ảnh ngang thì ta ocr luôn
                         if i.shape[0] < i.shape[1]:
                             i = Image.fromarray(i)
                             s, p = detector.predict(i, return_prob = True)
-                            if p > 0.6:
+                            if p > 0.7:
                                 ten_sach += ' ' + s
+                        #nếu ảnh dọc thì ta xoay ảnh đông thời dùng prob để ocr được kết quả tốt nhất
                         else:
                             im1 = Image.fromarray(i)
                             s1, p1 = detector.predict(im1, return_prob = True)
@@ -98,6 +107,8 @@ def craft_and_ocr(results):
                                 s = s3
                             if p > 0.6:
                                 ten_sach += ' ' + s
+                                
+                #tương tự những cái còn lại thêm text vảo chuỗi tên tác giả, nhà xuất bản, tập, người dịch, tái bản
                 elif key == 1:
                     for i in a:
                         if i.shape[0] < i.shape[1]:
@@ -238,6 +249,8 @@ def craft_and_ocr(results):
                                 s = s3
                             if p > 0.6:
                                 tai_ban += ' ' + s
+         
+        #nếu mà tên tác giả, nhà xuất bản, tập, người dịch, tái bản có lẫn vào tên sách thì lấy nó ra
         for i in cache:
             if i == 1:
                 if ten_tac_gia in ten_sach:
@@ -269,7 +282,8 @@ def craft_and_ocr(results):
                 else:
                     for s in tai_ban.split():
                         ten_sach = ten_sach.replace(s, '')
-
+        
+        #thêm vào dictionary
         features = {
             'tên sách': ten_sach,
             'tên tác giả': ten_tac_gia,
@@ -278,8 +292,9 @@ def craft_and_ocr(results):
             'người dịch': nguoi_dich,
             'tái bản': tai_ban
         }
-
+        
         out.append(features)
+    #tạo dataframe và lưu vào
     output = pd.DataFrame()
     output = output.append(out, ignore_index=True, sort=False)
 
